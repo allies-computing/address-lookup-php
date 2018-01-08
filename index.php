76b15b1c83dd535f5949a5928f627d93edd90150
Stephen Keable <stephen.keable@alliescomputing.com>
2018-01-08 12:55:44 +0000

    comment typos

--- index.php
+++ index.php
@@ -19,13 +19,13 @@
         
     } else {
         
-        echo "<p>Pass a postcode or part of and address using <code>?input=nr147pz</code></p>";
+        echo "<p>Pass a postcode or part of an address using <code>?input=nr147pz</code></p>";
         
     }
 
     function search_address($input = "", $country_code = "GB", $page = 0) {
         
-        // Replace with your API key, test key test key is locked to NR14 7PZ postcode search
+        // Replace with your API key, test key is locked to NR14 7PZ postcode search
         $api_key = "PCW45-12345-12345-1234X";
         
         // Grab the input text and trim any whitespace
@@ -44,11 +44,11 @@
             // Create the URL to API including API key and encoded address
             $address_url = "https://ws.postcoder.com/pcw/" . $api_key . "/address/".$country_code."/" . urlencode($input) . "?page=" . $page; 
             
-            // use cURL to send the request and get the output
+            // Use cURL to send the request and get the output
             $session = curl_init($address_url); 
             // Tell cURL to return the request data
             curl_setopt($session, CURLOPT_RETURNTRANSFER, true); 
-            // use application/json to specify json return values, the default is XML.
+            // Use application/json to specify json return values, the default is XML
             $headers = array('Content-Type: application/json');
             curl_setopt($session, CURLOPT_HTTPHEADER, $headers);
 
